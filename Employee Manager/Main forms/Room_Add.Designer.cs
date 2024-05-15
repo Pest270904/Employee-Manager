688b9f63bc2bfbd602284fb2c890a218247cfe99
xuanhathu <dangchithinhcungson@gmail.com>
2024-05-15 22:34:32 +0700

    update test

--- Employee Manager/Main forms/Room_Add.Designer.cs
+++ Employee Manager/Main forms/Room_Add.Designer.cs
@@ -1,4 +1,10 @@
-﻿namespace Employee_Manager.Main_forms
+﻿using Employee_Manager.Others;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows.Forms;
+using System;
+
+namespace Employee_Manager.Main_forms
 {
     partial class Room_Add
     {
@@ -18,6 +24,58 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+            if (String.IsNullOrEmpty(roomID_TB.Text) || String.IsNullOrEmpty(roomName_TB.Text))
+            {
+                MessageBox.Show("Please fill al the fields");
+            }
+            else
+            {
+                try
+                {
+                    FirebaseResponse respGet = client.Get("users/" + currentUser.username + "/rooms/");
+                    Dictionary<string, RoomData> result;
+
+                    if (respGet.Body != "null")
+                    {
+                        result = respGet.ResultAs<Dictionary<string, RoomData>>();
+                    }
+                    else
+                    {
+                        result = new Dictionary<string, RoomData>();
+                    }
+
+                    bool ifExist = false;
+
+                    // check if roomId is already exists for current user
+                    foreach (var get in result)
+                    {
+                        if (roomID_TB.Text == get.Value.roomID)
+                        {
+                            ifExist = true;
+                            break;
+                        }
+                    }
+
+                    if (!ifExist)
+                    {
+                        var dataLayer = new RoomData
+                        {
+                            roomID = roomID_TB.Text,
+                            roomName = roomName_TB.Text,
+                            countEmployee = 0
+                        };
+
+                        // add new room
+                        SetResponse resp = await client.SetTaskAsync("users/" + currentUser.username + "/rooms/r" + roomID_TB.Text, dataLayer);
+                        MessageBox.Show("New room added");
+
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Room ID is already exists");
+                    }
+                }
         }
 
         #region Windows Form Designer generated code
